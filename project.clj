af30690da752494915e481f468219fe3ccc06068
Morten Kristoffersen <morten.kristoffersen@ice.net>
2021-05-04 19:31:35 +0200

    Clojars groupId and project info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject plug-utils "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject net.clojars.mokr/plug-utils "0.1.0-SNAPSHOT"
+  :description "Small util functions for clj and/or cljs"
+  :url "https://github.com/mokr/plug-utils"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
