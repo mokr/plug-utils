4d7835f36373972a76b962fa69420c395f99c3f0
Morten Kristoffersen <morten.kristoffersen@ice.net>
2021-05-05 20:02:31 +0200

    Updated clojure dependency to 1.10.3

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/mokr/plug-utils"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [re-frame "1.2.0"]
                  [reagent "1.0.0"]]
   :repl-options {:init-ns plug-utils.re-frame})
