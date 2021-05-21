1a9e9eec3016ec18291fd277e5b6bdc9de367875
Morten Kristoffersen <morten.kristoffersen@ice.net>
2021-05-21 07:56:33 +0200

    Added clojurescript dependency. Marked all dependencies with: :scope "provided"

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/mokr/plug-utils"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [re-frame "1.2.0"]
-                 [reagent "1.0.0"]]
+  :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.844" :scope "provided"]
+                 [re-frame "1.2.0" :scope "provided"]
+                 [reagent "1.0.0" :scope "provided"]]
   :repl-options {:init-ns plug-utils.re-frame})
