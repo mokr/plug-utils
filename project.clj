509a2c0e589e93dea1d27cb1d141cf72773ee238
Morten Kristoffersen <morten.kristoffersen@ice.net>
2021-11-02 12:25:49 +0100

    Library updates

--- project.clj
+++ project.clj
@@ -4,8 +4,15 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3" :scope "provided"]
-                 [org.clojure/clojurescript "1.10.844" :scope "provided"]
-                 [org.clojure/core.async "1.3.618" :scope "provided"]
-                 [re-frame "1.2.0" :scope "provided"]
-                 [reagent "1.0.0" :scope "provided"]]
-  :repl-options {:init-ns plug-utils.re-frame})
+                 [org.clojure/clojurescript "1.10.879" :scope "provided"]
+                 [org.clojure/core.async "1.3.622" :scope "provided"]]
+  :repl-options {:init-ns plug-utils.re-frame}
+
+  :profiles
+  {:cljs
+   {:source-paths ["src" "test"]
+    :dependencies [[re-frame "1.2.0" :scope "provided"]
+                   [reagent "1.1.0" :scope "provided"]
+                   [thheller/shadow-cljs "2.15.12" :scope "provided"]]}}
+
+  )
